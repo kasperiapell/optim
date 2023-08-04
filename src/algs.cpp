e5f0bd05ed6f738b3e8383358b9d9f3de2982bef
kasperiapell <kasperi.apell@gmail.com>
2023-08-04 21:38:39 +0300

    add 1d newton method

--- src/algs.cpp
+++ src/algs.cpp
@@ -20,3 +20,25 @@ double gradientDescent(
 
         return estimate;
 }
+
+double newtonMethod(
+        double init_val, 
+        double alpha, 
+        unsigned iter_count, 
+        double (*objective)(double), 
+        double (*objective_der)(double),
+        double (*objective_second_der)(double)
+)
+{
+        double search_dir;
+        double step;
+        double estimate = init_val;
+
+        for (unsigned i = 0; i < iter_count, i++) {
+                search_dir = objective_der(estimate) / objective_second_der(estimate);
+                step = -1 * alpha * search_dir;
+                estimate += step;
+        }
+
+        return estimate;
+}
