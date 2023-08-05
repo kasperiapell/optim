e990ce2238551fc2df9a47b9ccc8fe70893474c0
kasperiapell <kasperi.apell@gmail.com>
2023-08-05 10:21:37 +0300

    generalize gd to n dimensions

--- src/algs.cpp
+++ src/algs.cpp
@@ -1,16 +1,19 @@
 // algs.cpp
+#include <Eigen/Dense>
 
-double gradientDescent(
-        double init_val, 
+using Eigen::MatrixXd;
+
+MatrixXd gradientDescent(
+        MatrixXd init_val, 
         double alpha, 
         unsigned iter_count, 
-        double (*objective)(double), 
-        double (*objective_der)(double)
+        MatrixXd (*objective)(MatrixXd), 
+        MatrixXd (*objective_der)(MatrixXd)
 )
 {
-        double search_dir;
-        double step;
-        double estimate = init_val;
+        MatrixXd search_dir;
+        MatrixXd step;
+        MatrixXd estimate = init_val;
 
         for (unsigned i = 0; i < iter_count; i++) {
                 search_dir = objective_der(estimate);
